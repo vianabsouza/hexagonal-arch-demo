97e851430bae3b02afdccd036868a5ed0074c2eb
Barbara Viana <vianabsouza@gmail.com>
2024-09-13 16:17:13 -0300

    refactor(hexagonal): use port interface to decouple crypto password logic

--- src/user/service/user-login.ts
+++ src/user/service/user-login.ts
@@ -1,5 +1,6 @@
 import CasoDeUso from "../../shared/use-case.ts";
 import ColecaoUsuario from "../data/user-collection.ts";
+import ProvedorCripto from "../model/cripto-provider.ts";
 import Usuario from "../model/user.ts";
 
 export type LoginUsuarioDTO = {
@@ -8,14 +9,16 @@ export type LoginUsuarioDTO = {
 }
 
 export default class LoginUsuario implements CasoDeUso<LoginUsuarioDTO, Usuario | null> {
+  constructor(private provedorCripto: ProvedorCripto) {}
   async executar(dto: LoginUsuarioDTO): Promise<Usuario | null> {
     const colecao = new ColecaoUsuario()
 
     const usuario = await colecao.buscarPorEmail(dto.email)
-    if(!usuario) return null
+    if(!usuario || !usuario.senha) return null
 
-    const senhaCriptografada = dto.senha.split('').reverse().join('')
-    if(usuario.senha !== senhaCriptografada) return null
+    const senhaCompara = await this.provedorCripto.comparar(dto.senha, usuario.senha)
+
+    if(!senhaCompara) return null
 
     return {
       nome: usuario.nome,
--- src/user/service/user-registration.ts
+++ src/user/service/user-registration.ts
@@ -1,10 +1,13 @@
-import CasoDeUso from "../../shared/use-case.ts";
+import CasoDeUso from "../../shared/use-case.ts"
 import ColecaoUsuario from "../data/user-collection.ts";
+import ProvedorCripto from "../model/cripto-provider.ts";
 import Usuario from "../model/user.ts";
 
 export default class RegistrarUsuario implements CasoDeUso<Required<Usuario>, void> {
+  constructor(private provedorCripto: ProvedorCripto) {}
+
   async executar(usuario: Required<Usuario>): Promise<void> {
-    const senhaCriptografada = usuario.senha.split('').reverse().join('')
+    const senhaCriptografada = await this.provedorCripto.criptografar(usuario.senha)
     const usuarioCriptografado = { ...usuario, senha: senhaCriptografada }
 
     const colecao = new ColecaoUsuario()
