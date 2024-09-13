1dcb35b69fae048a9ca409fa13900b0d28fe9036
Barbara Viana <vianabsouza@gmail.com>
2024-09-13 17:44:45 -0300

    refactor(hexagonal): rename user collection in memory class

--- src/core/user/data/user-collection-in-memory.ts
+++ src/core/user/data/user-collection-in-memory.ts
@@ -1,15 +1,15 @@
 import Usuario from "../model/user.ts";
 
-export default class ColecaoUsuario {
+export default class ColecaoUsuarioMemoria {
   static readonly usuarios: Usuario[] = []
 
   // deno-lint-ignore require-await
   async adicionar(usuario: Usuario): Promise<void> {
-    ColecaoUsuario.usuarios.push(usuario)
+    ColecaoUsuarioMemoria.usuarios.push(usuario)
   }
 
  // deno-lint-ignore require-await
  async buscarPorEmail(email: string): Promise<Usuario | null> {
-    return ColecaoUsuario.usuarios.find(usuario => usuario.email === email) ?? null
+    return ColecaoUsuarioMemoria.usuarios.find(usuario => usuario.email === email) ?? null
   }
 }
